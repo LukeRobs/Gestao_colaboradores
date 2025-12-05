cde577e805071e53a20a2d51561f29356bd7b74a
LukeRobs <lucassrobson06@gmail.com>
2025-12-04 22:32:58 -0300

    excluindo arquivo teste

--- backend/teste-admin.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const hashedPassword = "$2a$10$B9TjLMVZMEhnzItrIDlUDOowPiHYfi63uI0j5gn4W3oC35gms8ybS"; // senha do admin do seed
-const plainPassword = "admin123";
-
-bcrypt.compare(plainPassword, hashedPassword, (err, result) => {
-  if (err) throw err;
-  console.log("Senha válida?", result); // deve imprimir: true
-});
